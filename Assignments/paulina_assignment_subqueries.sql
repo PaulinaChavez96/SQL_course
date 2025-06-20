c2d73fdaa967dc9cc972b6745dd3f4421ec717ca
Paulina Chavez <131932491+PaulinaChavez96@users.noreply.github.com>
2025-06-20 12:58:21 -0600

    Add files via upload

--- Assignments/paulina_assignment_subqueries.sql
+++ Assignments/paulina_assignment_subqueries.sql
@@ -1,6 +1,16 @@
+-- ASSIGNMENT DESCRIPTION:
+-- Hello! Our Product Team plans on evaluating our product prices
+-- later this week to see if any adjustments need to be made for 
+-- next year.
+-- Can you give me a list of our products from the most to least
+-- expensive, along with how much each product differs from the 
+-- average unit price?
+-- Thanks! 
+-- Mandy
+
+
 USE maven_advanced_sql;
 
-SELECT product_id, product_name, unit_price, (SELECT AVG(unit_price) FROM products) AS avg_unit_price, unit_price - (SELECT AVG(unit_price) FROM products) AS unit_price_diff 
-		FROM products
+SELECT product_id, product_name, unit_price, (SELECT AVG(unit_price) FROM products) AS avg_unit_price, 
+		unit_price - (SELECT AVG(unit_price) FROM products) AS diff_price FROM products
 ORDER BY unit_price DESC;
-	
